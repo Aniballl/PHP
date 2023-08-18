9d3e1d9b786a81b804bbe5d5c9aad6225d466c35
Anibal <anibalggm@gmail.com>
2023-08-18 11:12:40 +0200

    Primera Versión

---
+++ Primer PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Primer PHP</title>
+</head>
+<body>
+    <?php
+        //Comentario de una linea
+        #Comentrario de una linea
+        /*Comentario de varias lineas*/
+
+        echo '<p>Hola Mundo </p>';
+        echo "Hola"," Aníbal"," bienvenido a PHP";
+        echo "macarrones";
+    ?>;
+    <br>
+<?='¿Como estas?' ?>
+</body>
+</html>
---
+++ Primer PHP/inicio.php
@@ -0,0 +1,18 @@
+<?php
+    $saludo='Hola PHP.';
+    $sitio='miSitio.com.';
+    echo 'Hola PHP.';
+    echo 'Bienvenido a miSitio.com.';
+	echo "macarrones";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+<body>
+    <p><?=$saludo;?> </p>
+    <p>Bienvenido a <?=$sitio;?> </p>
+</body>
+</html>
